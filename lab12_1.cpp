d2ab7ab167ccad8563741dd773c85da937cb328e
Ahiwamu <niceniceiloveyou@gmail.com>
2024-12-24 14:28:52 +0700

    finished

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
+long long fibonacci(int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
     return 0; 
+}
+
+long long fibonacci(int n){
+    if(n <= 1) return n;
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
