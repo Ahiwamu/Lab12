3b4562f59e37ee956c2fb9a68f0d7101bc6025c5
Ahiwamu <niceniceiloveyou@gmail.com>
2024-12-24 15:01:13 +0700

    finished

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename A>
+void mySwap(A &, A &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename type>
+void mySwap(type &x, type &y){
+	type tmp = x;
+	x = y;
+	y = tmp;
 }
 
